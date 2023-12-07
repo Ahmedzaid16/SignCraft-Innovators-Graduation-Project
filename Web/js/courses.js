d4b4fda0ac2a99cd21adac436da6475724eeba54
Sh6rif <sherifalex34@gmail.com>
2023-12-07 22:07:45 +0200

    added new course in Education and upload videos of two courses

--- Web/js/courses.js
+++ Web/js/courses.js
@@ -7,17 +7,17 @@ function scrollToElement(elementId) {
   element.scrollIntoView({ behavior: "smooth" });
 }
 
-// This Function Show or Hide an image Based on the Scroll Position
-function showImageOnScroll() {
-  // Define the scroll threshold (in pixels) to trigger the image visibility.
-  const scrollThreshold = 200;
-  // Check if the User has Scrolled beyond the defined threshold
-  const isScrolled = window.scrollY > scrollThreshold;
-  // Select the image element with the class "bottom-left-image"
-  const bottomLeftImage = document.querySelector(".bottom-left-image");
-  /* Set the opacity of the image based on the scroll condition 
-  If scrolled, set opacity to 1 (visible); otherwise, set it to 0 (hidden) */
-  bottomLeftImage.style.opacity = isScrolled ? 1 : 0;
-}
-// Add a scroll event listener to trigger the function when the user scrolls.
-window.addEventListener("scroll", showImageOnScroll);
+// // This Function Show or Hide an image Based on the Scroll Position
+// function showImageOnScroll() {
+//   // Define the scroll threshold (in pixels) to trigger the image visibility.
+//   const scrollThreshold = 200;
+//   // Check if the User has Scrolled beyond the defined threshold
+//   const isScrolled = window.scrollY > scrollThreshold;
+//   // Select the image element with the class "bottom-left-image"
+//   const bottomLeftImage = document.querySelector(".bottom-left-image");
+//   /* Set the opacity of the image based on the scroll condition
+//   If scrolled, set opacity to 1 (visible); otherwise, set it to 0 (hidden) */
+//   bottomLeftImage.style.opacity = isScrolled ? 1 : 0;
+// }
+// // Add a scroll event listener to trigger the function when the user scrolls.
+// window.addEventListener("scroll", showImageOnScroll);
